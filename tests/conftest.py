908917a422ecd0474020d05fb6bf208e3f0200b4
Vitaliy Sysolyatin <vitos282008@gmail.com>
2026-02-12 19:14:20 +0300

    fix: Iterator in conftest

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,11 +1,12 @@
 import pytest
 
+from collections.abc import Iterator
 from src.clients.base_client import BaseClient
 from src.config.settings import settings
 
 
 @pytest.fixture(scope="session")
-def api_client() -> BaseClient:
+def api_client() -> Iterator[BaseClient]:
     client = BaseClient(
         base_url=settings.base_url,
         timeout_seconds=settings.timeout_seconds,
